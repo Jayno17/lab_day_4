4a4e8fe7c08b337d54e74b194754a8b8cf218a72
howmehow <howmehow@gmail.com>
2020-01-16 14:39:22 +0000

    4 tasks done

--- friends.rb
+++ friends.rb
@@ -6,3 +6,15 @@ end
 def get_tv_show(person)
   return person[:favourites][:tv_show]
 end
+
+def likes_to_eat(person, snack)
+  if person[:favourites][:snacks].include?(snack)
+    return true
+  else
+    return false
+  end
+end
+
+def add_friend(person, new_friend)
+  person[:friends].push(new_friend)
+end
